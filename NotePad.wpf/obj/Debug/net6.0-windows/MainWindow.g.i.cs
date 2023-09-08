f10cb42297174e7c9dd3f30d7f71202778969c1b
Victor Van Volcem <victor.vanvolcem@gmail.com>
2023-09-08 15:23:20 +0200

    Update git ignore

--- NotePad.wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ NotePad.wpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -141,7 +141,7 @@ namespace NotePad.wpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NotePad.wpf;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NotePad.wpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
